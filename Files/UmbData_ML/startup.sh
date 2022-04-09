af4ee2e8bb4aa08229fa7c886c746cd8a61085fa
Carl Sargunar <carl@sargunar.com>
2022-04-09 17:04:52 +0100

    check for file so DB only gets created once

--- Files/UmbData_ML/startup.sh
+++ Files/UmbData_ML/startup.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 set -
 
-
 if [ "$1" = '/opt/mssql/bin/sqlservr' ]; then
-  # Delete any databases from previous runs
-  rm -f /var/opt/sqlserver/UmbracoDb.mdf
-  rm -f /var/opt/sqlserver/UmbracoDb_log.ldf
 
   # If this is the container's first run, initialize the application database
   if [ ! -f /tmp/app-initialized ]; then
     # Initialize the application database asynchronously in a background process. This allows a) the SQL Server process to be the main process in the container, which allows graceful shutdown and other goodies, and b) us to only start the SQL Server process once, as opposed to starting, stopping, then starting it again.
     function initialize_app_database() {
+
+      echo "Removing old data and creating new database"
+      # Delete any databases from previous runs
+      rm -f /var/opt/sqlserver/UmbracoDb.mdf
+      rm -f /var/opt/sqlserver/UmbracoDb_log.ldf
+
       # Wait a bit for SQL Server to start. SQL Server's process doesn't provide a clever way to check if it's up or not, and it needs to be up before we can import the application database
       sleep 15s
 
